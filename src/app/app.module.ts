258fc6687b2842f3e643013ebc97a83c652fee3e
Pavithra Ravi <pavithra931993@gmail.com>
2016-12-14 15:18:36 +0530

    [Changes] - First changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
